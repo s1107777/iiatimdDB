f5f4a2919528e63be55a7f9510c57e6b5feb5d05
s1107777 <s1107777@student.hsleiden.nl>
2021-07-01 09:44:01 +0200

    gemerged

--- routes/api.php
+++ routes/api.php
@@ -22,13 +22,7 @@ Route::group([
     Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
     Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
     Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
-<<<<<<< HEAD
-    // Route::post('/refresh', [App\Http\Controllers\AuthController::class, 'refresh']);
-    Route::get('/user-profile', [App\Http\Controllers\AuthController::class, 'userProfile']);  
-    Route::post('/change-profile', [App\Http\Controllers\AuthController::class, 'changeProfile']);  
-=======
     Route::post('/refresh', [App\Http\Controllers\AuthController::class, 'refresh']);
     Route::get('/user-profile', [App\Http\Controllers\AuthController::class, 'userProfile']);  
     Route::get('/movies', [App\Http\Controllers\MoviesController::class, 'movies']);  
->>>>>>> fb5e65db62feb3db348114d16b87a0937b943780
 });
